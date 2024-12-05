521905349787faed69d0f3bbf6c0268bd9e82809
PLANCHE Benoit <56308872+benoit-planche@users.noreply.github.com>
2024-12-05 23:02:33 +0100

    Poc compteur (#4)
    * Enhance Counter component with dynamic button spawning and time tracking
    
    * Update button variant and enhance spawn probability logic; add Counter component route
    
    * Add redux for counter
    
    * Enhance Counter component with dynamic button spawning, time tracking, and improved button management
    
    ---------
    
    Co-authored-by: pauline <pauline.contat01@etu.umontpellier.fr>

---
+++ src/CounterSauv.tsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [buttons, setButtons] = useState<{ id: number; top: number; left: number; }[]>([]);
+  const [time, setTime] = useState(0);
+  const [spawnProbability, setSpawnProbability] = useState(4000);
+
+  useEffect(() => {
+    const buttonInterval = setInterval(() => {
+      const newButton = {
+        id: Date.now(),
+        top: Math.random() * (window.innerHeight - 50),
+        left: Math.random() * (window.innerWidth - 100)
+      };
+      setButtons(prevButtons => [...prevButtons, newButton]);
+      
+
+      // Augmente la probabilité toutes les 20 secondes
+      if (time % 20 === 0 && time > 0 && spawnProbability > 500) {
+        setSpawnProbability(prev => prev / 2);
+      }
+    }, Math.random() * spawnProbability);
+
+    const timerInterval = setInterval(() => {
+      setTime(prev => prev + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(buttonInterval);
+      clearInterval(timerInterval);
+    };
+  }, [time, spawnProbability]);
+
+  const handleButtonClick = (buttonId: number) => {
+    setCount(prevCount => prevCount + 1);
+    setButtons(prevButtons => 
+      prevButtons.filter(button => button.id !== buttonId)
+    );
+  };
+
+  return (
+    <div style={{ position: 'relative', height: '100vh' }}>
+      <h1>Compteur: {count}</h1>
+      <h2>Time: {time} secondes</h2>
+      {buttons.map((button) => (
+        <button 
+          key={button.id}
+          onClick={() => handleButtonClick(button.id)}
+          style={{
+            position: 'absolute', 
+            top: button.top, 
+            left: button.left
+          }}
+        >
+          plastique
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default Counter;
