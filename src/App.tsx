7d3bed9f2e3a1c82a180bed214e193dcfa9be529
Dorian Grasset <dorian.grasset.contact@gmail.com>
2024-12-05 22:20:13 +0100

    remove app.css

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog-no-closed";
 import { Button } from "@/components/ui/button";
-import "./App.css";
 
 function App() {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
