8af5b50a903177ffc991798e4770bd61791a3118
Louis Labeyrie <labeyrielouis@gmail.com>
2024-12-06 00:31:25 +0100

    feat: add tests for Counter component and clean up existing test files
    Signed-off-by: Louis Labeyrie <labeyrielouis@gmail.com>

--- src/tests/checkbox.test.tsx
+++ src/tests/checkbox.test.tsx
@@ -1,7 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { Checkbox } from "@/components/ui/checkbox";
-import "@testing-library/jest-dom";
 
 describe("Checkbox Component", () => {
   it("toggles checked state on click", async () => {
