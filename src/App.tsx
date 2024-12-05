1fb831a0c966dc6a1d328e1a2a4ede644cba8ef3
Dorian Grasset <dorian.grasset.contact@gmail.com>
2024-12-05 22:16:11 +0100

    responsive homepage

--- src/App.tsx
+++ src/App.tsx
@@ -21,45 +21,26 @@ function App() {
   };
 
   return (
-    <div className="bg-blue-950 bg-deepwater h-dvh flex gap-64 flex-col items-center justify-center w-full bg-cover text-white font-pressstart">
-      <a
-        href="https://x.com/not_a_shower"
-        className="text-gray-500 hover:underline pt-4"
-      >
+    <div className="bg-blue-950 bg-deepwater h-dvh flex flex-col items-center justify-center w-full bg-cover text-white font-pressstart">
+      <a href="https://x.com/not_a_shower" className="text-gray-500 text-[10px] md:text-sm hover:underline pt-4">
         @not_a_shower
       </a>
-      <div className="h-full flex flex-col gap-72">
-        <div className="flex flex-row text-8xl justify-center items-center">
-          <div>RACE FOR W</div>
-          <img
-            className="object-contain h-[90px]"
-            src="src/assets/water-drop.png"
-            alt="Water Drop"
-          />
-          <div>TER</div>
+      <div className="h-full flex flex-col items-center justify-center gap-16 md:gap-72">
+        <div className="flex text-center flex-col text-6xl md:text-8xl justify-center items-center">
+          <div>RACE FOR</div>
+          <div className='flex flex-row'>
+            <div>W</div>
+            <img className="object-contain h-[60px] md:h-[90px] pb-2" src="src/assets/water-drop.png" alt="Water Drop" />
+            <div>TER</div>
+          </div>
         </div>
         <div className="flex flex-col gap-8 justify-center items-center">
-          <Button
-            variant={"homepage"}
-            size={"homepage"}
-            disabled={!isChecked}
-            className="w-fit relative bg-blue-400 rounded-lg border-none p-0 cursor-pointer outline-offset-4 active:outline-none"
-          >
-            <span className="block px-10 py-6 rounded-lg text-4xl text-white bg-blue-500 translate-y-[-6px] active:translate-y-[-2px]">
-              PLAY
-            </span>
+          <Button variant={'homepage'} size={'homepage'} disabled={!isChecked} className="w-fit relative bg-blue-400 rounded-lg border-none p-0 cursor-pointer outline-offset-4 active:outline-none">
+            <span className="block px-10 py-6 rounded-lg text-2xl md:text-4xl text-white bg-blue-500 translate-y-[-6px] active:translate-y-[-2px]">PLAY</span>
           </Button>
           <div className="flex items-center space-x-2 gap-4">
-            <Checkbox
-              id="terms"
-              className="border-white"
-              checked={isChecked}
-              onCheckedChange={handleCheckboxChange}
-            />
-            <label
-              htmlFor="terms"
-              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-            >
+            <Checkbox id="terms" className="border-white" checked={isChecked} onCheckedChange={handleCheckboxChange} />
+            <label htmlFor="terms" className="text-[10px] md:text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
               Accept terms and conditions
             </label>
           </div>
@@ -69,7 +50,7 @@ function App() {
       {/* Dialog */}
       <Dialog open={isDialogOpen} onOpenChange={() => {}}>
         <DialogContent
-          className="sm:max-w-[425px]"
+          className="md:max-w-[425px]"
           onEscapeKeyDown={(e) => e.preventDefault()} // Bloque la fermeture avec Escape
           onPointerDownOutside={(e) => e.preventDefault()} // Bloque la fermeture en cliquant à l'extérieur
         >
