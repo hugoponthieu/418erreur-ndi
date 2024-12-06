d2e8ae3814a6a157c3d98902aa22866a03f1933a
Louis Labeyrie <labeyrielouis@gmail.com>
2024-12-06 02:27:17 +0100

    feat: add Babel configuration and update Jest settings for improved testing and coverage reporting
    Signed-off-by: Louis Labeyrie <labeyrielouis@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 export default {
 	preset: 'ts-jest',
 	testEnvironment: 'jsdom',
+	moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
 	moduleNameMapper: {
 		'^@/(.*)$': '<rootDir>/src/$1',
 		'\\.(css|less|scss|sass)$': 'identity-obj-proxy',
@@ -8,9 +9,10 @@ export default {
 	setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 	transform: {
 		'^.+\\.(ts|tsx)$': 'ts-jest',
+		'^.+\\.[tj]sx?$': 'babel-jest',
 	},
 	testPathIgnorePatterns: ['/node_modules/', '/dist/'],
 	collectCoverage: true,
 	coverageDirectory: 'coverage',
-	coverageReporters: ['html', 'text',"json-summary"],
+	coverageReporters: ['html', 'text', 'json-summary'],
 };
--- src/tests/button.test.tsx
+++ src/tests/button.test.tsx
@@ -1,17 +1,26 @@
-import { render, screen } from "@testing-library/react";
-import { Button } from "@/components/ui/button";
 import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Button } from "@/components/ui/button";
 
 describe("Button Component", () => {
-  it("renders correctly", () => {
+  it("renders the button with correct text", () => {
     render(<Button>Click me</Button>);
-    const buttonElement = screen.getByText(/click me/i);
-    expect(buttonElement).toBeInTheDocument();
+    expect(screen.getByText(/Click me/i)).toBeInTheDocument();
   });
 
-  it("applies custom classes", () => {
-    render(<Button className="custom-class">Custom Button</Button>);
-    const buttonElement = screen.getByText(/custom button/i);
-    expect(buttonElement).toHaveClass("custom-class");
+  it("triggers the onClick event handler when clicked", async () => {
+    const mockHandler = jest.fn();
+    render(<Button onClick={mockHandler}>Click me</Button>);
+    const button = screen.getByText(/Click me/i);
+    await userEvent.click(button);
+    expect(mockHandler).toHaveBeenCalledTimes(1);
+  });
+
+  it("disables the button when the disabled prop is true", () => {
+    render(<Button disabled>Disabled</Button>);
+    const button = screen.getByText(/Disabled/i);
+    expect(button).toBeDisabled();
   });
 });
+
