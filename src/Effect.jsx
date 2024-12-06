50dea93a08c50c6d5b4d1fda4b8ab2da2109be0c
Tristan-Mihai Radulescu <90451752+Courtcircuits@users.noreply.github.com>
2024-12-06 03:44:44 +0100

    feat/hud (#18)
    * yeah
    
    * yeah

--- src/Effect.jsx
+++ src/Effect.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useRef } from "react";
 import { useFrame, useThree } from "@react-three/fiber";
 import { EffectComposer, ShaderPass, RenderPass } from "three-stdlib";
+
 import PixelateShader from "./PixelateShader.ts";
+import { VignetteShader } from "./VignetteShader.ts";
+import ColorCorrectionShader from "./ColorCorrection.ts";
 import * as THREE from "three";
 
 export function Effects() {
@@ -11,15 +14,22 @@ export function Effects() {
   useEffect(() => {
     const renderTarget = new THREE.WebGLRenderTarget(size.width, size.height);
     const pixelatePass = new ShaderPass(PixelateShader);
+    const colorPass = new ShaderPass(ColorCorrectionShader);
+    const vignette = new ShaderPass(VignetteShader);
+    // colorPass.uniforms["addRGB"].value = new THREE.Vector3(0.2, 0, 0.4);
     pixelatePass.uniforms["resolution"].value = new THREE.Vector2(
       size.width,
-      size.height
+      size.height,
     );
     pixelatePass.uniforms["pixelSize"].value = 5.0;
+
+    vignette.uniforms["darkness"].value = 1.2;
 
     composer.current = new EffectComposer(gl, renderTarget);
     composer.current.addPass(new RenderPass(scene, camera));
     composer.current.addPass(pixelatePass);
+    // composer.current.addPass(colorPass);
+    composer.current.addPass(vignette);
 
     return () => {
       renderTarget.dispose();
---
+++ src/pages/Game.tsx
@@ -0,0 +1,44 @@
+import { ShopTable } from "@/components/ui/shop";
+import UnderwaterWorld from "../UnderwaterWorld";
+import { TemperatureIcon } from "@/components/icons/temperature";
+import { Gauge } from "@/components/ui/gauge";
+import { ToxicityIcon } from "@/components/icons/toxicity";
+import { OceanHeightIcon } from "@/components/icons/ocean_height";
+import { MoneyCounter } from "@/components/ui/money_counter";
+export function Game() {
+  return (
+    <div className="h-screen">
+      <div className="z-10 bg-transparent absolute right-5 top-5">
+        <ShopTable
+          title="GARBAGE"
+          items={[
+            {
+              name: "auto-garbage collector",
+              currentAmount: 1,
+              price: 10,
+            },
+          ]}
+        />
+      </div>
+      <div className="z-10 bg-transparent absolute left-5 top-5 flex flex-col gap-2">
+        <div className="flex flex-row gap-3 items-center">
+          <ToxicityIcon color="#00FF4D" size={30} />
+          <Gauge color="green" label="Toxicity" percentage={30} />
+        </div>
+        <div className="flex flex-row gap-3 items-center">
+          <OceanHeightIcon color="#92DCE5" size={30} />
+          <Gauge color="blue" label="Ocean's height" percentage={30} />
+        </div>
+        <div className="flex flex-row gap-3 items-center">
+          <TemperatureIcon color="#FF66D8" size={30} />
+          <Gauge color="pink" label="Temperature" percentage={30} />
+        </div>
+      </div>
+
+      <div className="z-10 bg-transparent absolute left-5 bottom-5 flex flex-col gap-2">
+        <MoneyCounter count={0} />
+      </div>
+      <UnderwaterWorld />
+    </div>
+  );
+}
